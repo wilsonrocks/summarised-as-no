e7f34ed12e3d96a6949256743f95b5c46627f07c
James Wilson <jwilsoncalderdale@gmail.com>
2019-08-06 21:05:27 +0100

    reads a list of URLs from a file

--- scrape.py
+++ scrape.py
@@ -2,23 +2,29 @@ import requests
 from bs4 import BeautifulSoup
 from pprint import pprint
 
-html = requests.get('https://bbc.co.uk/news').content
-parsed = BeautifulSoup(html, features='lxml')
+output = []
 
-for match in parsed.find_all():
-    if match.name != 'a':
-        match.replaceWithChildren()
+with open('./sites') as sites:
+    for site in sites:
+        print(f'scraping {site}')
 
-all_links = parsed.find_all('a')
+        html = requests.get('https://bbc.co.uk/news').content
+        parsed = BeautifulSoup(html, features='lxml')
+
+        for match in parsed.find_all():
+            if match.name != 'a':
+                match.replaceWithChildren()
+
+        all_links = parsed.find_all('a')
+
+        question_links = (
+            [link for link in all_links if link.text.endswith('?')])
+
+        for link in question_links:
+            output.append({
+                'url': link['href'],
+                'title': link.string,
+            })
 
 
-question_links = ([link for link in all_links if link.text.endswith('?')])
-
-for link in question_links:
-    attrs = dict(link.attrs)
-    for attr in attrs:
-        if attr != 'href':
-            del link.attrs[attr]
-
-
-pprint(question_links)
+pprint(output)
